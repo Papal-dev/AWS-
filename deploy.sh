f2dd7c56e7a8d755602c6b278dfa12e7b5fb7634
kbabila93 <k.babila@yahoo.com>
2025-12-02 15:56:11 -0500

    this is my final commit

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 Application is ready to be deloyed to the  development  environment.
 development  must be reviewed by manager before deployment
 
+development must be reviewed by managers before deployment
