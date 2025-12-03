5dbb485b36d4fde69af9225c44a89ef48a56ea33
kbabila93 <babilak@yahoo.com>
2025-12-02 20:20:02 -0500

    Update deployment notes in deploy.sh
    Added a note about the application's market readiness timeline.

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@ Application is ready to be deloyed to the  development  environment.
 development  must be reviewed by manager before deployment
 
 development must be reviewed by managers before deployment
+This application will be read for the market in three weeks 
